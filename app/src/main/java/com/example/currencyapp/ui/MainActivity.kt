8afe41ce08ba77c88f355843e549953b7d1a18e9
YuliantiOenang <yuliantioenang@gmail.com>
2022-10-16 02:29:03 +0800

    Update layout

--- app/src/main/java/com/example/currencyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyapp/ui/MainActivity.kt
@@ -31,7 +31,6 @@ import javax.inject.Inject
 class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.View {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
-//    @Inject lateinit var mainFragmentFactory: MainFragmentFactory
     @Inject
     lateinit var presenter: MainContract.Presenter
     @Inject
@@ -50,20 +49,6 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.Vie
         binding.rv.adapter = adapter
         binding.rv.layoutManager = GridLayoutManager(this@MainActivity, 2)
 
-//        setSupportActionBar(binding.toolbar)
-//        binding.name.text = viewModel.name
-//        binding.button.setOnClickListener { viewModel.userClicked() }
-//        supportFragmentManager.fragmentFactory = mainFragmentFactory
-//        val navController = findNavController(R.id.nav_host_fragment_content_main)
-//        appBarConfiguration = AppBarConfiguration(navController.graph)
-//        setupActionBarWithNavController(navController, appBarConfiguration)
-
-//        binding.fab.setOnClickListener { view ->
-//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                .setAction("Action", null).show()
-//        }
-
-
         binding.button.setOnClickListener {
             if (binding.spinner.selectedItem != null && binding.etNominal.text.isNotEmpty()) {
                 (binding.etNominal.text.toString()).toDoubleOrNull()?.let { it ->
@@ -77,22 +62,18 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.Vie
     }
 
     override fun showLoading() {
-        Log.d("YULI", "showLoading")
         loading.show()
     }
 
     override fun hideLoading() {
-        Log.d("YULI", "hideLoading")
         loading.hide()
     }
 
     override fun showRetry() {
-        Log.d("YULI", "showRetry")
         TODO("Not yet implemented")
     }
 
     override fun showError(error: String?) {
-        Log.d("YULI", "showError:"+error)
         Toast.makeText(this, error, Toast.LENGTH_LONG).show()
     }
 
@@ -100,9 +81,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.Vie
         val dataAdapter: ArrayAdapter<String> =
             ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies)
 
-
         dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-
 
         binding.spinner.setAdapter(dataAdapter)
     }
