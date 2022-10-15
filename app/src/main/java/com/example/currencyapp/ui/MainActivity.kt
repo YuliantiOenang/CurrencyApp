4e7805cb1439e57138829a0e2d95b4ba6af12ccc
YuliantiOenang <yuliantioenang@gmail.com>
2022-10-15 20:19:30 +0800

    Use activity instead of fragment

--- app/src/main/java/com/example/currencyapp/ui/MainActivity.kt
+++ app/src/main/java/com/example/currencyapp/ui/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.currencyapp.ui
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
@@ -18,13 +20,15 @@ import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main), MainContract.View {
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
 //    @Inject lateinit var mainFragmentFactory: MainFragmentFactory
-
+    @Inject
+    lateinit var presenter: MainContract.Presenter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        presenter.initializeData()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
@@ -38,13 +42,44 @@ class MainActivity : AppCompatActivity() {
 //        binding.name.text = viewModel.name
 //        binding.button.setOnClickListener { viewModel.userClicked() }
 //        supportFragmentManager.fragmentFactory = mainFragmentFactory
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        appBarConfiguration = AppBarConfiguration(navController.graph)
-        setupActionBarWithNavController(navController, appBarConfiguration)
+//        val navController = findNavController(R.id.nav_host_fragment_content_main)
+//        appBarConfiguration = AppBarConfiguration(navController.graph)
+//        setupActionBarWithNavController(navController, appBarConfiguration)
 
 //        binding.fab.setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                .setAction("Action", null).show()
 //        }
+    }
+
+    override fun showLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun hideLoading() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showRetry() {
+        TODO("Not yet implemented")
+    }
+
+    override fun showError(error: String?) {
+        Toast.makeText(this, error, Toast.LENGTH_LONG).show()
+    }
+
+    override fun showAvailableCurrency(currencies: List<String>) {
+        val dataAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencies)
+
+
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+
+        binding.spinner.setAdapter(dataAdapter)
+    }
+
+    override fun showCalculatedOtherCurrency(otherCurrencies: List<ExchangeRate>) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/com/example/currencyapp/ui/di/MainModule.kt
+++ app/src/main/java/com/example/currencyapp/ui/di/MainModule.kt
@@ -1,36 +1,34 @@
 package com.example.currencyapp.ui.di
 
+import android.app.Activity
 import androidx.fragment.app.Fragment
 import com.example.currencyapp.ui.CurrencyPresenter
+import com.example.currencyapp.ui.MainActivity
 import com.example.currencyapp.ui.MainContract
 import com.example.currencyapp.ui.MainFragment
 import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 interface MainModule {
     @Binds
-    fun provideView(mainFragment: MainFragment): MainContract.View
+    fun provideView(mainFragment: MainActivity): MainContract.View
 
     @Binds
     fun providePresenter(presenter: CurrencyPresenter): MainContract.Presenter
 }
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ActivityComponent::class)
 object MainProviderModule {
     @Provides
-    fun provideMainFragment(fragment: Fragment): MainFragment {
-        return fragment as MainFragment
-    }
-
-    @Provides
-    fun provideMainFragment(context: ApplicationContext): Fragment {
-        return context
+    fun provideActivity(activity: Activity): MainActivity {
+        return activity as MainActivity
     }
 }
