5af15b64f456e852450d0011e9a9aa677c671556
YuliantiOenang <yuliantioenang@gmail.com>
2022-12-27 21:44:49 +0800

    Add workflow file

--- app/src/main/java/com/example/currencyapp/sharedPreferences/di/LocalDataStoreModule.kt
+++ app/src/main/java/com/example/currencyapp/sharedPreferences/di/LocalDataStoreModule.kt
@@ -9,7 +9,7 @@ import dagger.hilt.components.SingletonComponent
 
 @Module
 @InstallIn(SingletonComponent::class)
-interface LocalDataStoreModule {
+interface DLocalDataStoreModule {
     @Binds
     fun bindCurrencyExchangeLocalDataStore(sharedPreferenceLocalDataStore: SharedPreferenceLocalDataStore): CurrencyExchangeLocalDataStore
 }
